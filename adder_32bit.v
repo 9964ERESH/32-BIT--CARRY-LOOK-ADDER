a574cff3b2b6f0cf7cd6710702e63750e43ff669
9964ERESH <144584265+9964ERESH@users.noreply.github.com>
2023-09-15 09:24:30 +0530

    Add files via upload

---
+++ adder_32bit.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08.09.2023 22:28:56
+// Design Name: 
+// Module Name: adder_32bit
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module adder_32bit(
+
+    );
+endmodule
